033a0878051190cf637a767191670964c1adcf7d
Nycolas Felipe <nycolasfelipe.contato@gmail.com>
2024-10-26 19:38:13 -0300

    Update obsidian_sync.cmd

--- obsidian_sync.cmd
+++ obsidian_sync.cmd
@@ -1,18 +1,22 @@
-:: Acessar o diretório
-cd "obsidian-domine-web"
+:: Put in here your obsidian vault directory name
+cd "your_obsidian_vault"
 
-:: Recuperar data e horário
+:: Retrieve date and time
+:: Default formatted as dd-mm-yy and 23:59
+:: Replace first line with "(set mydate=%%b-%%a-%%c)" to use USA format (mm-dd-yy)
 For /f "tokens=1-3 delims=/ " %%a in ('date /t') do (set mydate=%%a-%%b-%%c)
 For /f "tokens=1-2 delims=/:" %%a in ("%TIME%") do (set mytime=%%a:%%b)
 
-:: Preparar mensagem do commit
+:: Prepare commit message
+:: Example: "sync 26-10-2024 - 19:02"
 set commit_msg="sync %mydate% - %mytime%"
 
-:: Commit do repositório
+:: Add all changes and commit with custom message
 git add *
 git commit -m %commit_msg%
 
-:: Push do repositório
+:: Push all changes to repository
 git push
 
+:: [Optional line] Prevent cmd window from closing to view result log
 cmd /k
